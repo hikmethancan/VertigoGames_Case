e87b57bc69a2f98e64b985c02847b9a3fa06e24e
Hikmethan Can <hikmethann@gmail.com>
2024-07-01 16:15:46 +0300

    [Added] Base Death Panel Setup

---
+++ Assets/_Main/Scripts/UserInterface/Buttons/Concrete/ExitButton.cs
@@ -0,0 +1,33 @@
+using _Main.Scripts.Signals;
+using _Main.Scripts.UserInterface.Buttons.Abstract;
+
+namespace _Main.Scripts.UserInterface.Buttons.Concrete
+{
+    public class ExitButton : ButtonsBase
+    {
+        protected override void OnButtonClicked()
+        {
+            SpinAction();
+        }
+
+        protected override void Register(bool isActive)
+        {
+            base.Register(isActive);
+            if (isActive)
+                GameSignals.OnReadyForSpinning += ReadyForSpin;
+            else
+                GameSignals.OnReadyForSpinning -= ReadyForSpin;
+        }
+
+        private void ReadyForSpin()
+        {
+            SetInteractable(true);
+        }
+
+        private void SpinAction()
+        {
+            SetInteractable(false);
+            GameSignals.OnSpinningButtonClicked?.Invoke();
+        }
+    }
+}
